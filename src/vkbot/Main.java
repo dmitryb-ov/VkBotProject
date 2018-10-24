57580eb1bc12127c7e7c00894a9e21b464646dfa
BLG51 <zary1998@gmail.com>
2018-10-24 16:56:13 +0300

    added libs

--- src/vkbot/Main.java
+++ src/vkbot/Main.java
@@ -2,8 +2,6 @@ package vkbot;
 
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.GroupActor;
-import com.vk.api.sdk.exceptions.ApiException;
-import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
 import javafx.application.Application;
 import org.eclipse.jetty.server.Server;
@@ -14,10 +12,21 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    private final static String PROPERTY_NAME = "config.properties";
+    public static void main(String[] args) throws Exception {
         Properties properties = readProperties();
         HttpTransportClient client = new HttpTransportClient();
         VkApiClient apiClient = new VkApiClient(client);
+
+        GroupActor actor = initVkApi(apiClient, readProperties());
+        BotRequestHandler botHandler = new BotRequestHandler(apiClient, actor);
+
+        Server server = new Server(8080);
+
+        server.setHandler(new RequestHandler(botHandler, properties.getProperty("confirmationCode")));
+
+        server.start();
+        server.join();
 
     }
 
@@ -33,7 +42,7 @@ public class Main {
     }
 
     private static Properties readProperties() throws FileNotFoundException {
-        InputStream in = Application.class.getClassLoader().getResourceAsStream("config.properties");
+        InputStream in = Application.class.getClassLoader().getResourceAsStream(PROPERTY_NAME);
         if (in == null) {
             throw new FileNotFoundException("dam son");
         }
